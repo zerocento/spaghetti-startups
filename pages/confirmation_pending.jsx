0a5b124b45719d0b01d61adb1b0dd32a771dd16d
Lorenzo Pilla <infopillingoita@gmail.com>
2023-02-03 16:55:41 +0100

    feat:  added email sent page

--- pages/confirmation_pending.jsx
+++ pages/confirmation_pending.jsx
@@ -1,3 +1,4 @@
+import { Box, Center, Flex, Image, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -5,11 +6,41 @@ export default function confirmation_pending() {
   const router = useRouter();
   const propValue = router.query.prop;
   return (
-    <div>
-      Grazie per esserti iscritto a Spaghetti Startups. Una mail di conferma è
-      stata inviata all'indirizzo {propValue}, clicca sul link per confermare il
-      tuo indirizzo email ed entrare in Spaghetti Startups. Se non vedi una mail
-      all'indirizzo indicato, controlla lo Spam.{' '}
-    </div>
+    <Flex
+      height={'100vh'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+    >
+      <Image src="/img/email_sent_icon.svg" w={['171px', '450px']} mb="5" />
+      <Text
+        as={'h1'}
+        fontWeight="700"
+        fontSize={['31px', '39px']}
+        lineHeight={['47px', '59px']}
+        textAlign="center"
+      >
+        Ci Sei quasi!
+      </Text>
+      <Box maxW={'80%'}>
+        <Text
+          fontWeight="700"
+          fontSize={'16'}
+          lineHeight="24px"
+          textAlign="center"
+        >
+          Abbiamo inviato una mail di conferma al tuo indirizzo
+        </Text>
+        <Text
+          fontWeight="400"
+          fontSize={'16'}
+          lineHeight="24px"
+          textAlign={'center'}
+        >
+          Clicca sul link nella mail per confermare il tuo account ed entrare in
+          Spaghetti Startups!
+        </Text>
+      </Box>
+    </Flex>
   );
 }
